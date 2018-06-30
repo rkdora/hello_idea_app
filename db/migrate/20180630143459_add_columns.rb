451660ef449f00d8a74567bc8489a878fd3658f3
Ryuto Kawano <q111011r@mail.kyutech.jp>
2018-06-30 23:39:05 +0900

    Add columns validates

---
+++ db/migrate/20180630143459_add_columns.rb
@@ -0,0 +1,7 @@
+class AddColumns < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :email, :string
+    add_column :posts, :content, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_29_091044) do
+ActiveRecord::Schema.define(version: 2018_06_30_143459) do
 
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "content"
   end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password"
+    t.string "name"
+    t.string "email"
   end
 
 end
